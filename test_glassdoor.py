bda0603296861965693efd80a50cb4af8a4c5104
Himanshu Shah <himanshu1410@live.com>
2014-07-21 20:29:55 -0400

    Fetching More Information on Companies

--- test_glassdoor.py
+++ test_glassdoor.py
@@ -1,24 +1,11 @@
 from bs4 import BeautifulSoup
-import requests                                                                 
+import requests
+import xlsxwriter
                 
 ## Global list of urls to be parsed
 url_list = []
 site_name = "http://www.glassdoor.com"
-
-def parse_url(url):
-    '''
-    Function adds the next links in the list of urls to be parsed.
-    Parses the company details on the page and stores them in database.
-    '''
-    r = requests.get(url)                                                           
-    soup = BeautifulSoup(r.content)
-#    links = soup.find_all('a')                                                      
-    g_data = soup.find_all('a', {"class": "item org emphasizedLink"}) 
-    for data in g_data:
-        print data.text
-    rating_data = soup.find_all("span", {"class": "gdRatingValueBar"})
-    for data in rating_data:
-        print data.text
+counter = 0
 
 def begin_parsing(init_url, minimum):
 
@@ -36,8 +23,9 @@ def begin_parsing(init_url, minimum):
         counter = counter + 1
         parse(url_to_parse, minimum)
 
-    print "-"*10
-    print url_list 
+    print "-"*20
+    print "Total number of results returned : %d" % (counter)
+    print "-"*20
 
 def parse(url_to_parse, minimum):
     global url_list
@@ -49,12 +37,51 @@ def parse(url_to_parse, minimum):
         nl = site_name + a
         if nl not in url_list:
             url_list.append(nl)
-    
+
+    # Company Names
     Companies = soup.find_all('a', {"class": "item org emphasizedLink"}) 
-    Ratings = soup.find_all("span", {"class": "gdRatingValueBar"})
+    
+    # Company Ratings
+    Ratings = soup.find_all("span", {"class": "gdRatingValueBar"}) 
+   
+    # Company headquaters
+    HeadQuaters = soup.find_all("span", {"class": "hqInfo adr"}) 
+
+    # Company Size
+    # Company CEO
+    CEOs = soup.find_all("p", {"class": "ceoData"}) 
+
+    # Founding Date: Growth %
+
+    ## Write Data to Excel Sheet ##
+    workbook = xlsxwriter.Workbook('demo.xlsx')
+    worksheet = workbook.add_worksheet()
+    bold = workbook.add_format({'bold': True})
+    # Writing Columns
+    worksheet.write('A1', 'No', bold)
+    worksheet.write('B1', 'Company Name', bold)
+    worksheet.write('C1', 'Rating', bold)
+    worksheet.write('D1', 'HeadQuater', bold)
+    worksheet.write('E1', 'CEO', bold)
+    worksheet.write('F1', 'CEO Approval', bold)
+    workbook.close()
+    ###############################
+    global counter
     for i in range(0,len(Companies)):
         if(float(Ratings[i].text) > minimum):
-            print "> %s %s" % (Companies[i].text,Ratings[i].text)
+            counter = counter + 1
+            print "%d %s %s %s %s %s " % \
+                    (counter, Companies[i].text,\
+                     Ratings[i].text,\
+                     HeadQuaters[i].find("span", {"class" : "value i-loc"}).text,\
+                     CEOs[i].find("span", {"class":"fn notranslate"}).text,\
+                     CEOs[i].find("span", {"class": "approvalPercent"}).text)
+#            worksheet.write(i-1, 0, Companies[i].text)
+#            worksheet.write(i-1, 1, Ratings[i].text)
+#            worksheet.write(i-1, 2, HeadQuaters[i].text)
+#            worksheet.write(i-1, 3, CEOs[i].text)
+#            worksheet.write(i-1, 4, 'A5', 'CEO Approval', bold)
+                                     
 
 def main():
     url = "http://www.glassdoor.com/Reviews/information-technology-reviews-SRCH_II10013.0,22.htm"
